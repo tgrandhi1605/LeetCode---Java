d1b98e7ff215e087f691889dcb112ad2c5545c67
Tharun Kumar Grandhi <tgrandhi@twilio.com>
2025-08-02 13:57:44 +0100

    SOlutions on 02/08

--- src/HashMaps/HappyNumber.java
+++ src/HashMaps/HappyNumber.java
@@ -1,2 +1,35 @@
-package HashMaps;public class HappyNumber {
+package HashMaps;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HappyNumber {
+    public static boolean isHappyNumber(int n) {
+        Set<Integer> seen = new HashSet<>();
+
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = sumOfDigits(n);
+        }
+
+        return n == 1;
+    }
+
+    public static int sumOfDigits(int number) {
+        int sum = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            sum += digit * digit;
+            number /= 10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isHappyNumber(1));
+        System.out.println(isHappyNumber(2));
+        System.out.println(isHappyNumber(3));
+        System.out.println(isHappyNumber(19));
+        System.out.println(isHappyNumber(152));
+    }
 }
--- src/SlidingWindow/LongestRepeatingCharacterReplacement.java
+++ src/SlidingWindow/LongestRepeatingCharacterReplacement.java
@@ -1,2 +1,33 @@
-package SlidingWindow;public class LongestRepeatingCharacterReplacement {
+package SlidingWindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestRepeatingCharacterReplacement {
+    public static int characterReplacement(String s, int k) {
+        Map<Character, Integer> frequencyMap = new HashMap<>();
+        int left = 0;
+        int maxCount = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char rightChar = s.charAt(right);
+            frequencyMap.put(rightChar, frequencyMap.getOrDefault(rightChar, 0) + 1);
+
+            maxCount = Math.max(maxCount, frequencyMap.get(rightChar));
+
+            while((right - left + 1) - maxCount > k) {
+                char leftChar = s.charAt(left);
+                frequencyMap.put(leftChar, (frequencyMap.get(leftChar) - 1));
+                left++;
+            }
+
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(characterReplacement("ABAB", 2));
+    }
 }
